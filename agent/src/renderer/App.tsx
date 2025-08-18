76572426d9d1b7fb05a971dcd440b6bfbce40367
Kaio Bruno <bmkaiobruno@gmail.com>
2025-08-18 12:03:06 -0300

    refactor(reseting code): reseting code to branch 607a4f0

--- agent/src/renderer/App.tsx
+++ agent/src/renderer/App.tsx
@@ -6,6 +6,8 @@ const App: React.FC = () => {
   return (
     <main className='h-screen flex items-start justify-center pt-20'>
       <div className='flex flex-col items-center'>
+
+        <h1 className='text-3xl font-bold mb-10'>Hello</h1>
         <div className='flex items-center gap-4'>
           <Link to="/dashboard" className='text-blue-600 font-medium hover:underline'>Dashboard</Link>
           <Link to="/admin" className='text-blue-600 font-medium hover:underline'>Admin</Link>
