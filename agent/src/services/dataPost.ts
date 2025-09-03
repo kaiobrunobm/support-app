deeae6581b094c745e00196d1c01632c4241f6b5
Kaio Bruno <bmkaiobruno@gmail.com>
2025-09-03 15:10:15 -0300

    chore(vercel settings): changing route of the versel.json

--- agent/src/services/dataPost.ts
+++ agent/src/services/dataPost.ts
@@ -12,7 +12,7 @@ export const startPostData = () => {
   cron.schedule('*/30 * * * * *', async () => {
     try {
       console.log('Collecting system info...');
-      const result = await sendToAPI(`http://localhost:${PORT}`);
+      const result = await sendToAPI(`https://support-app-backend.vercel.app/`);
       console.log('Data sent successfully:', result);
     } catch (err) {
       console.error('Error sending system info:', err);
