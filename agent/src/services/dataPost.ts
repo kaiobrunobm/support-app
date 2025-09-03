81bffd3c25741e918e0779a589fddea2aae72b5f
Kaio Bruno <bmkaiobruno@gmail.com>
2025-09-03 15:34:10 -0300

    chore(rollback vercel): taking of the setting to deploy on vercel

--- agent/src/services/dataPost.ts
+++ agent/src/services/dataPost.ts
@@ -12,7 +12,7 @@ export const startPostData = () => {
   cron.schedule('*/30 * * * * *', async () => {
     try {
       console.log('Collecting system info...');
-      const result = await sendToAPI(`https://support-app-backend.vercel.app/`);
+      const result = await sendToAPI(`http://localhost:${PORT}`);
       console.log('Data sent successfully:', result);
     } catch (err) {
       console.error('Error sending system info:', err);
