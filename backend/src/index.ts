a6fbad2025a1c58fef6a7873b315212b83351507
Kaio Bruno <bmkaiobruno@gmail.com>
2025-09-04 09:29:37 -0300

    fix(index.ts route fiz): fixing route of the index.ts express app of /api/system-info to /system/inf

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
 import systemRoutes from "./modules/system.routes";
 
 dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient();
-const PORT = process.env.PORT || 4000;
+
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/system-info", systemRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.use("/system-info", systemRoutes);
 
 export default app
