498df2555fc5a6e6fcaa67b2c3206ac7ceae42cd
Kaio Bruno <bmkaiobruno@gmail.com>
2025-09-11 15:55:32 -0300

    style(search bar style): changing the style of the search bar and the list of systems

--- agent/src/renderer/screens/components/SystemSearch.tsx
+++ agent/src/renderer/screens/components/SystemSearch.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useAppContext } from "../../../utils/ContextProvider";
+import { DesktopTowerIcon, DotOutlineIcon } from "@phosphor-icons/react";
 
 const SystemSearch: React.FC = () => {
   const { setSystemInfo, user } = useAppContext();
@@ -45,7 +46,7 @@ const SystemSearch: React.FC = () => {
   if (user?.role !== "ADMIN") return null; // ✅ only admins
 
   return (
-    <div className="relative w-64">
+    <div className="relative w-[420px]">
       <input
         type="text"
         value={query}
@@ -53,16 +54,24 @@ const SystemSearch: React.FC = () => {
         placeholder="Pesquisar sistema..."
         className="w-full px-4 py-3 border-2 border-border rounded-md outline-none bg-background/50 text-text ring-0"
       />
-      {loading && <div className="bg-background absolute top-full left-0 p-2 text-sm">Carregando...</div>}
+      {loading && <DotOutlineIcon size={96} weight="fill" className="animate-pulse text-text absolute top-16 left-40"/>}
       {results.length > 0 && (
-        <ul className="absolute top-full left-0 w-full bg-background border-border rounded shadow-lg mt-1 z-50">
-          {results.map((sys) => (
+        <ul className="absolute top-full left-[-50px] w-[500px] bg-background border border-border rounded-md shadow-lg mt-1 z-50">
+          {results.map((sys, index) => (
             <li
               key={sys.id}
-              className="p-2 transition-all duration-150 ease-in-out hover:bg-border cursor-pointer"
+              className={`flex gap-4 px-6 py-2 transition-all duration-150 ease-in-out hover:bg-border/20 cursor-pointer ${index > 0 && 'border-t border-border'}`}
               onClick={() => handleSelect(sys.id)}
             >
-              {sys.hostname} {sys.network.adapters[0].ip}
+              <DesktopTowerIcon size={40} weight="fill" className="text-text text-sm"/>
+              <div className="flex items-center gap-4">
+                <span>{sys.hostname}</span>
+                <div className="h-1/2 w-[1px] bg-border"/>
+                 <span>{sys.network.adapters[0].ip}</span>
+                 <div className="h-1/2 w-[1px] bg-border"/>
+                  <span>{sys.users[0].username}</span>
+              </div>
+              
             </li>
           ))}
         </ul>
